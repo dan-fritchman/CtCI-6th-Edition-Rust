36034d3510b7c7e062cf9d194cda2b2d2aa21213
Dan Fritchman <dan@fritch.mn>
2021-11-02 09:36:19 -0700

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! 
+//! # Cracking the Coding Interview Crate 
+//! 
+//! Each chapter's solutions are contained in an internal module named `chapter_{num}`. 
+//! 
+
+
+#[test]
+fn alive() {}
+
+
