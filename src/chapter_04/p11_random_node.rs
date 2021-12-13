8e27dd8922cd0df156f4cfcbd2f2d7289b031d91
Dan Fritchman <dan@fritch.mn>
2021-12-13 15:01:57 -0800

    Chapter 4 - Trees & Graphs

--- src/chapter_04/p11_random_node.rs
+++ src/chapter_04/p11_random_node.rs
@@ -1,42 +1,260 @@
 //!
 //! # Random Node:
 //!
-//! You are implementing a binary tree class from scratch which, in addition to insert, find, and delete, has a method getRandomNode()
-//! which returns a random node from the tree.
+//! You are implementing a binary tree class from scratch which, in addition to insert, find, and delete,
+//! has a method getRandomNode() which returns a random node from the tree.
 //! All nodes should be equally likely to be chosen.
 //! Design and implement an algorithm for getRandomNode, and explain how you would implement the rest of the methods.
 //!
 //! Hints: #42, #54, #62, #75, #89, #99, #7 72, #7 79
 //!
 
-#[cfg(test)]
-use crate::binary_tree::Error;
-use crate::binary_tree::{BinaryTree, NodePtr};
+// This models McDowell's "Option #7", probably the most-optimal solution.
+// Each node keeps track of the size of its sub-tree.
+// A random integer between 0 and `root.size` dictates which node
+// is randomly selected.
 
-pub fn get_random_node<T>(_tree: &BinaryTree<T>) -> NodePtr<T> {
-    todo!()
+use std::ops::{Index, IndexMut};
+
+///
+/// # (Yet) Another Binary Tree Node!
+///
+/// This time with a size of its sub-tree.
+///
+#[derive(Debug)]
+pub struct Node {
+    pub data: isize,
+    pub size: usize,
+    pub index: NodeIndex,
+    pub left: Option<NodeIndex>,
+    pub right: Option<NodeIndex>,
+    pub parent: Option<NodeIndex>,
+}
+impl Node {
+    /// Create a new [Node] with no children, and no parent
+    pub fn orphan(data: isize, index: NodeIndex) -> Self {
+        Self {
+            data,
+            size: 1,
+            index,
+            left: None,
+            right: None,
+            parent: None,
+        }
+    }
 }
 
-#[ignore]
+/// Index Reference into a Tree
+#[derive(Debug, Clone, Copy)]
+pub struct NodeIndex(usize);
+
+/// Binary Tree
+#[derive(Debug)]
+pub struct BinarySearchTree {
+    pub nodes: Vec<Node>,
+    pub head: Option<NodeIndex>,
+}
+impl Default for BinarySearchTree {
+    fn default() -> Self {
+        Self {
+            nodes: Vec::new(),
+            head: None,
+        }
+    }
+}
+impl Index<NodeIndex> for BinarySearchTree {
+    type Output = Node;
+    fn index(&self, index: NodeIndex) -> &Node {
+        &self.nodes[index.0]
+    }
+}
+impl IndexMut<NodeIndex> for BinarySearchTree {
+    fn index_mut(&mut self, index: NodeIndex) -> &mut Node {
+        &mut self.nodes[index.0]
+    }
+}
+
+impl BinarySearchTree {
+    /// Add a new [Node] with value `data`. Returns its index.
+    fn add_node(&mut self, data: isize) -> NodeIndex {
+        let index = NodeIndex(self.nodes.len());
+        self.nodes.push(Node {
+            data,
+            index,
+            size: 1,
+            parent: None,
+            left: None,
+            right: None,
+        });
+        index
+    }
+    /// Insert a new [Node] with value `val`. Returns its [NodeIndex].
+    pub fn insert(&mut self, val: isize) -> Result<NodeIndex, Error> {
+        // Create and add the new node
+        let newindex = self.add_node(val);
+
+        let mut parent = match self.head {
+            Some(p) => p,
+            None => {
+                // First node. Make it our `head` pointer.
+                self.head = Some(newindex);
+                return Ok(newindex);
+            }
+        };
+        loop {
+            self[parent].size += 1;
+            if val < self[parent].data {
+                // Look left
+                match self[parent].left {
+                    Some(idx) => parent = idx, // Node will be somewhere in `idx`'s sub-tree
+                    None => {
+                        // No child here, insert new node in place
+                        self[newindex].parent = Some(parent);
+                        self[parent].left = Some(newindex);
+                        break;
+                    }
+                }
+            } else {
+                // Look right
+                match self[parent].right {
+                    Some(idx) => parent = idx, // Node will be somewhere in `idx`'s sub-tree
+                    None => {
+                        // No child here, insert new node in place
+                        self[newindex].parent = Some(parent);
+                        self[parent].right = Some(newindex);
+                        break;
+                    }
+                }
+            }
+        }
+        return Ok(newindex);
+    }
+    /// Find the node with value `val`, if present
+    pub fn find(&self, val: isize) -> Option<NodeIndex> {
+        let mut node = self.head;
+        while let Some(idx) = node {
+            if self[idx].data == val {
+                return Some(idx);
+            }
+            node = if self[idx].data > val {
+                self[idx].left
+            } else {
+                self[idx].right
+            };
+        }
+        None // Not found
+    }
+    /// Get a uniformly-distributed random [NodeIndex].
+    /// Returns `None` if the tree is empty.
+    pub fn get_random(&self) -> Option<NodeIndex> {
+        // Check for emptiness, and get our root-node.
+        let head = match self.head {
+            None => return None,
+            Some(h) => h,
+        };
+
+        // Grab a random, uniformly-distributed integer,
+        // within the bounds of our `head`'s size.
+        let random_num = {
+            use rand::distributions::{Distribution, Uniform};
+
+            let mut rng = rand::thread_rng();
+            let dist = Uniform::from(0..self[head].size);
+            dist.sample(&mut rng)
+        };
+
+        // And kick off the recursive search for the `random_num`-th
+        // node in an in-order traversal.
+        Some(self.random_helper(head, random_num))
+    }
+    /// Recursive helper for `get_random`.
+    /// Returns the `rand`-th in-order node of the sub-tree headed by `node`.
+    /// Panics if the sub-tree has less than `rand` nodes.
+    fn random_helper(&self, node: NodeIndex, rand: usize) -> NodeIndex {
+        if rand == 0 {
+            return node; // Base case: `node` is it.
+        }
+        // If the node has a left-chld with enough children, recurse into it
+        if let Some(lf) = self[node].left {
+            if self[lf].size >= rand {
+                return self.random_helper(lf, rand - 1);
+            }
+        }
+        // Otherwise peel out the left-sub-tree size, and recurse into the right
+        let left_size = match self[node].left {
+            Some(l) => self[l].size,
+            None => 0,
+        };
+        self.random_helper(self[node].right.unwrap(), rand - left_size - 1)
+    }
+}
+
+/// Local Error Type
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Error;
+
+/// Solution Implementation
+/// Returns a random [NodeIndex] from `tree`.
+/// Panics if `tree` is empty.
+pub fn get_random_node(tree: &BinarySearchTree) -> NodeIndex {
+    tree.get_random().unwrap()
+}
+
 #[test]
 fn test_get_random_node() -> Result<(), Error> {
     use std::collections::HashMap;
 
-    let mut bst = BinaryTree::default();
-    let n = bst.insert(20, None)?;
-    let n = bst.insert(9, Some(n))?;
-    let n = bst.insert(25, Some(n))?;
-    let n = bst.insert(5, Some(n))?;
-    let n = bst.insert(12, Some(n))?;
-    let n = bst.insert(11, Some(n))?;
-    let _n = bst.insert(14, Some(n))?;
-    // let n = bst.delete(12); // FIXME!
+    let mut bst = BinarySearchTree::default();
+    let n20 = bst.insert(20)?;
+    assert_eq!(bst[n20].size, 1);
 
+    let n09 = bst.insert(9)?;
+    assert_eq!(bst[n20].size, 2);
+    assert_eq!(bst[n09].size, 1);
+
+    let n25 = bst.insert(25)?;
+    assert_eq!(bst[n20].size, 3);
+    assert_eq!(bst[n09].size, 1);
+    assert_eq!(bst[n25].size, 1);
+
+    let n05 = bst.insert(5)?;
+    assert_eq!(bst[n20].size, 4);
+    assert_eq!(bst[n09].size, 2);
+    assert_eq!(bst[n25].size, 1);
+    assert_eq!(bst[n05].size, 1);
+
+    let n12 = bst.insert(12)?;
+    assert_eq!(bst[n20].size, 5);
+    assert_eq!(bst[n09].size, 3);
+    assert_eq!(bst[n25].size, 1);
+    assert_eq!(bst[n05].size, 1);
+    assert_eq!(bst[n12].size, 1);
+
+    let n11 = bst.insert(11)?;
+    assert_eq!(bst[n20].size, 6);
+    assert_eq!(bst[n09].size, 4);
+    assert_eq!(bst[n25].size, 1);
+    assert_eq!(bst[n05].size, 1);
+    assert_eq!(bst[n12].size, 2);
+    assert_eq!(bst[n11].size, 1);
+
+    let n14 = bst.insert(14)?;
+    assert_eq!(bst[n20].size, 7);
+    assert_eq!(bst[n09].size, 5);
+    assert_eq!(bst[n25].size, 1);
+    assert_eq!(bst[n05].size, 1);
+    assert_eq!(bst[n12].size, 3);
+    assert_eq!(bst[n11].size, 1);
+    assert_eq!(bst[n14].size, 1);
+
+    // We don't have great statistical tests for the uniformity of
+    // random-node generation here, nor do we really want to spend
+    // the run time checking them. But we can grab a random node
+    // a sizable handful of times, and check that nothing goes terribly wrong.
     let mut counts = HashMap::new();
     for _ in 0..100 {
         let node = get_random_node(&bst);
-        let data = node.borrow().data;
-        *counts.entry(data).or_insert(0) += 1;
+        *counts.entry(node.0).or_insert(0) += 1;
     }
     Ok(())
 }
--- src/chapter_04/p12_paths_with_sum.rs
+++ src/chapter_04/p12_paths_with_sum.rs
@@ -8,15 +8,66 @@
 //! Hints: #6, #74, #52, #68, #77, #87, #94, #703, #708, #115
 //!
 
-use crate::binary_tree::BinaryTree;
 #[cfg(test)]
 use crate::binary_tree::Error;
+use crate::binary_tree::{BinaryTree, NodePtr};
 
-pub fn paths_with_sum(_tree: &BinaryTree<isize>, _sum: isize) -> usize {
-    todo!()
+/// Primary Implementation
+///
+/// Track each path leading to each node, incrementing the paths-count where needed,
+/// and and appending the node's value.
+///
+/// FIXME: this is closer to McDowell's "brute force" solution than the "optimized" hashmap-baseed on.
+/// It's not clear that the hashmap solution actually does what the problem-statement asks,
+/// particularly depending whether the paths which must add to `sum` must be
+/// continuous or not.
+/// There are no specific examples (with solutions) in McDowell,
+/// so this point lacks some clarity.
+/// There will certainly be cases in which this solution differs from the hashmap-one on this basis.
+/// For example in a totally unbalanced tree of the form:
+/// tree = 11 => 1 => 10
+/// where "=>" can represent either "left child" or "right child".
+/// Does `paths_with_sum(tree, 21)` equal one or zero?
+/// In this case, zero. In the hashmap solution, one.
+///
+pub fn paths_with_sum(tree: &BinaryTree<isize>, sum: isize) -> usize {
+    let paths = vec![];
+    helper(&tree.head, &paths, sum)
 }
+/// Recursive helper
+fn helper(node: &Option<NodePtr<isize>>, paths: &Vec<Vec<isize>>, sum: isize) -> usize {
+    let node = match node {
+        Some(n) => n,     // Unwrap the node-pointer
+        None => return 0, // Base case: null node-pointer
+    };
+    let node_data = node.borrow().data;
 
-#[ignore]
+    // Count up matches ending at this node
+    let my_matches = paths
+        .iter()
+        .filter(|path| {
+            let s: isize = path.iter().sum();
+            s + node_data == sum
+        })
+        .count();
+
+    // Append `node` to each entry of `paths`
+    let mut child_paths: Vec<Vec<isize>> = paths
+        .iter()
+        .map(|s| {
+            let mut s = s.to_vec();
+            s.push(node_data);
+            s
+        })
+        .collect();
+    // And add a new path with `node` as the sole entry
+    child_paths.push(vec![node_data]);
+
+    // Total up this node's matches with those from each child
+    my_matches
+        + helper(&node.borrow().left, &child_paths, sum)
+        + helper(&node.borrow().right, &child_paths, sum)
+}
 #[test]
 fn test_paths_with_sum() -> Result<(), Error> {
     let mut t1 = BinaryTree::default();
@@ -32,8 +83,16 @@ fn test_paths_with_sum() -> Result<(), Error> {
     let n10 = t1.insert(8, Some(n9))?;
     let _11 = t1.insert(-8, Some(n10))?;
 
-    assert_eq!(paths_with_sum(&t1, 8), usize::MAX);
-    assert_eq!(paths_with_sum(&t1, 6), usize::MAX);
+    assert_eq!(paths_with_sum(&t1, 8), 4); // FIXME: verify these values offline
+    assert_eq!(paths_with_sum(&t1, 6), 2); // FIXME: verify these values offline
+
+    // Test case from the comments above
+    let mut t1 = BinaryTree::default();
+    let n1 = t1.insert(11, None)?;
+    let n2 = t1.insert(1, Some(n1.clone()))?;
+    let n3 = t1.insert(10, Some(n2.clone()))?;
+    assert_eq!(paths_with_sum(&t1, 12), 1);
+    assert_eq!(paths_with_sum(&t1, 21), 0);
 
     Ok(())
 }
